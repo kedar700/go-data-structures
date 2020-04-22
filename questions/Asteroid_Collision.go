58b247ebd45c9a7207dd4833cabc6b9f7a7b88b1
kedar700 <kedarnaik16@gmail.com>
2020-04-22 09:55:28 -0500

    fix: Updated the asteroid collision problem

--- questions/Asteroid_Collision.go
+++ questions/Asteroid_Collision.go
@@ -1,49 +1,10 @@
 package questions
 
-/*
-public int[] asteroidCollision(int[] asteroids) {
-        LinkedList<Integer> s = new LinkedList<>();
-        for (int i : a) {
-            if (i > 0)
-                s.add(i);
-            else {
-                while (!s.isEmpty() && s.getLast() > 0 && s.getLast() < -i)
-                    s.pollLast();
-                if (!s.isEmpty() && s.getLast() == -i)
-                    s.pollLast();
-                else if (s.isEmpty() || s.getLast() < 0)
-                    s.add(i);
-            }
-        }
-        return s.stream().mapToInt(i->i).toArray();
-    }
-  answers := make([]int, 0)
-
-    for _, v := range asteroids{
-        keep := true
-        for len(answers) > 0 && v < 0 && answers[len(answers)-1] > 0{
-            if answers[len(answers)-1] < -v{
-                answers = answers[:len(answers)-1]
-                continue
-            }else if answers[len(answers)-1] == -v{
-                answers = answers[:len(answers)-1]
-            }
-            keep = false
-            break
-        }
-
-        if keep{
-            answers = append(answers, v)
-        }
-    }
-    return answers
-*/
-
 func AsteroidCollision(asteroids []int) []int {
 	fList := make([]int, 0)
 	for _, asteroidVal := range asteroids {
 		keep := true
-		for len(fList) > 0 && asteroidVal < 0 && fList[len(fList)-1] > 0 { // if the asteroid value is negetive and the last element is negetive
+		for len(fList) > 0 && asteroidVal < 0 && fList[len(fList)-1] > 0 { // if the asteroid value is negetive and the last stored element is positive
 			if fList[len(fList)-1] < -asteroidVal {
 				fList = fList[:len(fList)-1]
 				continue
